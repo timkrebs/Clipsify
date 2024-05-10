210764b6d6f264c9650c375a3a2e02c8f19e3c5a
timkrebs <timkrebs9@gmail.com>
2024-05-10 09:30:58 +0200

    chore: Update email.py to use os.getenv for EMAIL_CONNECTION_STRING

--- app/notification/send/email.py
+++ app/notification/send/email.py
@@ -1,18 +1,17 @@
 import json
-from os import environ as env
+import os
 
 from azure.communication.email import EmailClient
-from dotenv import find_dotenv, load_dotenv
 
-ENV_FILE = find_dotenv()
-if ENV_FILE:
-    load_dotenv(ENV_FILE)
+#ENV_FILE = find_dotenv()
+#if ENV_FILE:
+#    load_dotenv(ENV_FILE)
 
 
 def notification(message):
     try:
         # Setup for the email client using the connection string from environment variables
-        connection_string = env.get("EMAIL_CONNECTION_STRING")
+        connection_string = os.getenv("EMAIL_CONNECTION_STRING")
         client = EmailClient.from_connection_string(connection_string)
 
         # Parse the incoming message JSON
