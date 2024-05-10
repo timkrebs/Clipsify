f8fbb55db09e10606dcd276a671861442565a197
timkrebs <timkrebs9@gmail.com>
2024-05-10 17:06:40 +0200

    chore: Update email.py to include HTML content in mail message

--- app/notification/send/email.py
+++ app/notification/send/email.py
@@ -1,12 +1,6 @@
 import json
 import os
-
 from azure.communication.email import EmailClient
-
-#ENV_FILE = find_dotenv()
-#if ENV_FILE:
-#    load_dotenv(ENV_FILE)
-
 
 def notification(message):
     try:
@@ -19,15 +13,48 @@ def notification(message):
         mp3_fid = message["mp3_fid"]
         receiver_address = message["username"]
 
-        # Construct the mail message content
+        # Prepare the HTML content
+        html_content = f"""
+        <html>
+          <head>
+            <style type="text/css">
+              .ExternalClass,.ExternalClass div,.ExternalClass font,.ExternalClass p,.ExternalClass span,.ExternalClass td,img {{line-height: 100%;}}#outlook a {{padding: 0;}}.ExternalClass,.ReadMsgBody {{width: 100%;}}a,blockquote,body,li,p,table,td {{-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;}}table,td {{mso-table-lspace: 0;mso-table-rspace: 0;}}img {{-ms-interpolation-mode: bicubic;border: 0;height: auto;outline: 0;text-decoration: none;}}table {{border-collapse: collapse !important;}}#bodyCell,#bodyTable,body {{height: 100% !important;margin: 0;padding: 0;font-family: ProximaNova, sans-serif;}}#bodyCell {{padding: 20px;}}#bodyTable {{width: 600px;}}
+            </style>
+          </head>
+          <body>
+            <center>
+              <table style='width: 600px;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;margin: 0;padding: 0;font-family: "ProximaNova", sans-serif;border-collapse: collapse !important;height: 100% !important;' align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
+                <tr>
+                  <td align="center" valign="top" id="bodyCell" style='-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;margin: 0;padding: 20px;font-family: "ProximaNova", sans-serif;height: 100% !important;'>
+                    <div class="main">
+                      <p style="text-align: center;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%; margin-bottom: 30px;">
+                        <img src="https://cdn.auth0.com/styleguide/2.0.9/lib/logos/img/badge.png" width="50" alt="Your logo goes here" style="-ms-interpolation-mode: bicubic;border: 0;height: auto;line-height: 100%;outline: none;text-decoration: none;">
+                      </p>
+                      <h1>Welcome to Clipsify!</h1>
+                      <p>Here is your Download ID: {mp3_fid}. Please click the following link to download your file:</p>
+                      <p><a href="https://clipsify.net/download?fileId={mp3_fid}">Download MP3</a></p>
+                      <br />
+                      Thanks!
+                      <br />
+                      <strong>Clipsify Team</strong>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+            </center>
+          </body>
+        </html>
+        """
+
+        # Construct the mail message content with HTML
         mail_message = {
             "senderAddress": "DoNotReply@clipsify.net",
             "recipients": {
                 "to": [{"address": receiver_address}],
             },
             "content": {
-                "subject": "Test Email",
-                "plainText": "Here is your Donwload ID:  " + mp3_fid,
+                "subject": "Your MP3 Conversion is Ready!",
+                "html": html_content,
             },
         }
 
@@ -38,3 +65,4 @@ def notification(message):
 
     except Exception as ex:
         print(ex)
+
