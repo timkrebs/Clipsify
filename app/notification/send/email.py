38cd83dc685696fd2bdbc275e35bff9aeffb7a2b
timkrebs <timkrebs9@gmail.com>
2024-05-11 17:12:07 +0200

    Update email template to highlight the Download ID and improve styling

--- app/notification/send/email.py
+++ app/notification/send/email.py
@@ -18,20 +18,38 @@ def notification(message):
         <html>
           <head>
             <style type="text/css">
-              .ExternalClass,.ExternalClass div,.ExternalClass font,.ExternalClass p,.ExternalClass span,.ExternalClass td,img {{line-height: 100%;}}#outlook a {{padding: 0;}}.ExternalClass,.ReadMsgBody {{width: 100%;}}a,blockquote,body,li,p,table,td {{-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;}}table,td {{mso-table-lspace: 0;mso-table-rspace: 0;}}img {{-ms-interpolation-mode: bicubic;border: 0;height: auto;outline: 0;text-decoration: none;}}table {{border-collapse: collapse !important;}}#bodyCell,#bodyTable,body {{height: 100% !important;margin: 0;padding: 0;font-family: ProximaNova, sans-serif;}}#bodyCell {{padding: 20px;}}#bodyTable {{width: 600px;}}
+              .ExternalClass, .ExternalClass div, .ExternalClass font, .ExternalClass p, .ExternalClass span, .ExternalClass td, img {line-height: 100%;}
+              #outlook a {padding: 0;}
+              .ExternalClass, .ReadMsgBody {width: 100%;}
+              a, blockquote, body, li, p, table, td {-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}
+              table, td {mso-table-lspace: 0; mso-table-rspace: 0;}
+              img {-ms-interpolation-mode: bicubic; border: 0; height: auto; outline: 0; text-decoration: none;}
+              table {border-collapse: collapse !important;}
+              #bodyCell, #bodyTable, body {height: 100% !important; margin: 0; padding: 0; font-family: ProximaNova, sans-serif;}
+              #bodyCell {padding: 20px;}
+              #bodyTable {width: 600px;}
+
+              /* Additional Styles for FID */
+              .fid-highlight {
+                font-weight: bold;
+                background-color: #f2f2f2; /* Light grey background */
+                padding: 10px;
+                border-radius: 5px;
+                display: inline-block;
+              }
             </style>
           </head>
           <body>
             <center>
-              <table style='width: 600px;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;margin: 0;padding: 0;font-family: "ProximaNova", sans-serif;border-collapse: collapse !important;height: 100% !important;' align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
+              <table style='width: 600px; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; mso-table-lspace: 0pt; mso-table-rspace: 0pt; margin: 0; padding: 0; font-family: "ProximaNova", sans-serif; border-collapse: collapse !important; height: 100% !important;' align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
                 <tr>
-                  <td align="center" valign="top" id="bodyCell" style='-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;margin: 0;padding: 20px;font-family: "ProximaNova", sans-serif;height: 100% !important;'>
+                  <td align="center" valign="top" id="bodyCell" style='-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; mso-table-lspace: 0pt; mso-table-rspace: 0pt; margin: 0; padding: 20px; font-family: "ProximaNova", sans-serif; height: 100% !important;'>
                     <div class="main">
-                      <p style="text-align: center;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%; margin-bottom: 30px;">
-                        <img src="https://cdn.auth0.com/styleguide/2.0.9/lib/logos/img/badge.png" width="50" alt="Your logo goes here" style="-ms-interpolation-mode: bicubic;border: 0;height: auto;line-height: 100%;outline: none;text-decoration: none;">
+                      <p style="text-align: center; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; margin-bottom: 30px;">
+                        <img src="https://cdn.auth0.com/styleguide/2.0.9/lib/logos/img/badge.png" width="50" alt="Your logo goes here" style="-ms-interpolation-mode: bicubic; border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none;">
                       </p>
                       <h1>Welcome to Clipsify!</h1>
-                      <p>Here is your Download ID: {mp3_fid}. Please click the following link to download your file:</p>
+                      <p>Here is your Download ID: <span class="fid-highlight">{mp3_fid}</span>. Please click the following link to download your file:</p>
                       <p><a href="http://clipsify.net/download?fid={mp3_fid}">Download MP3</a></p>
                       <br />
                       Thanks!
@@ -44,6 +62,7 @@ def notification(message):
             </center>
           </body>
         </html>
+
         """
 
         # Construct the mail message content with HTML
